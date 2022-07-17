f66fb3d481291a715adf3a0a7ead05d556a9635e
Piotr <ppasieka@gmail.com>
2022-07-17 08:47:05 +0000

    implement pesel validation + tests

--- pesel.go
+++ pesel.go
@@ -1,34 +1,125 @@
 package pesel
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type Gender string
+type PeselNumber string
+
+const (
+	Female Gender = "female"
+	Male   Gender = "male"
+)
 
 type Date struct {
-	year  uint
-	month uint
-	day   uint
+	Year  int
+	Month time.Month
+	Day   int
 }
 
 type Pesel struct {
-	number      string
-	gender      string
+	number      PeselNumber
+	gender      Gender
 	dateOfBirth Date
 }
 
 type peselError struct {
 	number string
+	reason string
 }
 
 func (pe peselError) Error() string {
-	return fmt.Sprintf("Invalid PESEL: %s", pe.number)
+	return fmt.Sprintf("Invalid PESEL : %s. %s", pe.number, pe.reason)
+}
+
+func toSlice(number string) ([]int, error) {
+	peselNumbers := make([]int, 11)
+	var parseError error = nil
+	for i := 0; i < 11; i++ {
+		digit, err := strconv.Atoi(string(number[i]))
+		if err != nil {
+			parseError = err
+			break
+		}
+		peselNumbers[i] = digit
+	}
+
+	return peselNumbers, parseError
+}
+
+var weights = [10]int{1, 3, 7, 9, 1, 3, 7, 9, 1, 3}
+
+func calculateChecksum(peselNumbers []int) int {
+	var sum int
+
+	for i, digit := range peselNumbers[:10] {
+		sum += digit * weights[i]
+	}
+
+	digit := sum % 10
+
+	if digit == 0 {
+		return 0
+	}
+	return 10 - digit
+}
+
+func decodeGender(peselNumbers []int) Gender {
+	if peselNumbers[9]%2 == 0 {
+		return Female
+	} else {
+		return Male
+	}
+}
+
+var centuries = [5]int{1900, 2000, 2100, 2200, 1800}
+
+func decodeDateOfBirth(peselNumbers []int) (Date, error) {
+	var birthDate = &Date{}
+	month := 10*peselNumbers[2] + peselNumbers[3]
+	mod := month / 20
+	date, err := time.Parse("20060102", fmt.Sprintf("%04d%02d%02d", centuries[mod]+10*peselNumbers[0]+peselNumbers[1], month-mod*20, 10*peselNumbers[4]+peselNumbers[5]))
+
+	if err != nil {
+		return *birthDate, err
+	}
+
+	birthDate.Year = date.Year()
+	birthDate.Month = date.Month()
+	birthDate.Day = date.Day()
+
+	return *birthDate, nil
 }
 
 func New(number string) (Pesel, error) {
-	var pesel Pesel
-	peselError := peselError{number: number}
+	var pesel = &Pesel{}
 
 	if len(number) != 11 {
-		return pesel, peselError
+		return *pesel, peselError{number: number, reason: "Invalid length"}
 	}
 
-	return pesel, peselError
+	peselNumbers, err := toSlice(number)
+	if err != nil {
+		return *pesel, peselError{number: number, reason: "Characters are invalid"}
+	}
+
+	checksum := calculateChecksum(peselNumbers)
+
+	if checksum != peselNumbers[10] {
+		return *pesel, peselError{number: number, reason: "Checksum does not match"}
+	}
+
+	dateOfBirth, dateErr := decodeDateOfBirth(peselNumbers)
+	if dateErr != nil {
+		return *pesel, peselError{number: number, reason: "Encoded date is invalid"}
+	}
+
+	pesel.number = PeselNumber(number)
+	pesel.gender = decodeGender(peselNumbers)
+	pesel.dateOfBirth = dateOfBirth
+
+	return *pesel, nil
 }
--- pesel_test.go
+++ pesel_test.go
@@ -4,9 +4,106 @@ import (
 	"testing"
 )
 
-func Test(t *testing.T) {
-	_, e := New("123")
-	if e == nil {
-		t.Error("Should fail when crating a PESEL")
+func TestPeselInvalidLength(t *testing.T) {
+	cases := []string{
+		"0628255417",
+		"06",
+		"",
+		"0628255417312",
+	}
+
+	for _, data := range cases {
+		_, err := New(data)
+		if err == nil {
+			t.Errorf("PESEL %s length is invalid", data)
+		}
+	}
+}
+
+type checksumTestData struct {
+	pesel            string
+	expectedChecksum int
+}
+
+func TestChecksumCalculation(t *testing.T) {
+	cases := []checksumTestData{
+		{pesel: "06282554176", expectedChecksum: 6},
+		{pesel: "95012037841", expectedChecksum: 1},
+		{pesel: "50052453448", expectedChecksum: 8},
+		{pesel: "57081443219", expectedChecksum: 9},
+		{pesel: "89092243964", expectedChecksum: 4},
+		{pesel: "59090217216", expectedChecksum: 6},
+		{pesel: "02242225878", expectedChecksum: 8},
+		{pesel: "49111167646", expectedChecksum: 6},
+		{pesel: "03211217445", expectedChecksum: 5},
+		{pesel: "85091728442", expectedChecksum: 2},
+	}
+
+	for _, data := range cases {
+		number, _ := toSlice(data.pesel)
+		checksum := calculateChecksum(number)
+		if checksum != data.expectedChecksum {
+			t.Errorf("Invalid checksum for PESEL %s. Expected %d, but got %d", data.pesel, data.expectedChecksum, checksum)
+		}
+	}
+}
+
+func TestValidPeselNumbers(t *testing.T) {
+	cases := []string{
+		"06282554176",
+		"95012037841",
+		"50052453448",
+		"57081443219",
+		"89092243964",
+		"59090217216",
+		"02242225878",
+		"49111167646",
+		"03211217445",
+		"85091728442",
+	}
+	for _, data := range cases {
+		_, err := New(data)
+		if err != nil {
+			t.Errorf("PESEL %s should be valid", data)
+		}
+	}
+}
+
+func TestInvalidPeselNumbers(t *testing.T) {
+	cases := []string{
+		"12345678403",
+	}
+	for _, data := range cases {
+		_, err := New(data)
+		if err == nil {
+			t.Errorf("PESEL %s should be invalid", data)
+		}
+	}
+}
+
+type genderTestData struct {
+	pesel          string
+	expectedGender Gender
+}
+
+func TestGenderIsEncoded(t *testing.T) {
+	cases := []genderTestData{
+		{pesel: "06282554176", expectedGender: "male"},
+		{pesel: "95012037841", expectedGender: "female"},
+		{pesel: "50052453448", expectedGender: "female"},
+		{pesel: "57081443219", expectedGender: "male"},
+		{pesel: "89092243964", expectedGender: "female"},
+		{pesel: "59090217216", expectedGender: "male"},
+		{pesel: "02242225878", expectedGender: "male"},
+		{pesel: "49111167646", expectedGender: "female"},
+		{pesel: "03211217445", expectedGender: "female"},
+		{pesel: "85091728442", expectedGender: "female"},
+	}
+
+	for _, data := range cases {
+		pesel, _ := New(data.pesel)
+		if pesel.gender != data.expectedGender {
+			t.Errorf("Invalid  for PESEL %s. Expected %s, but got %s", data.pesel, data.expectedGender, pesel.gender)
+		}
 	}
 }
